41a0574e2f41dd7904eead81ffc950c469135945
YunQi0102 <93147911+YunQi0102@users.noreply.github.com>
2021-11-10 00:42:37 +0800

    Update and rename StarArrowR.java to StarArrow.java

--- src/main/java/com/yun/StarArrow.java
+++ src/main/java/com/yun/StarArrow.java
@@ -1,10 +1,10 @@
 package com.yun;
 
-public class StarArrowR {
+public class StarArrow {
     public static void main(String[] args) {
         for (int i = 0; i < 11; i++) {
             for (int k = 0; k < 21; k++) {
-                if ((2*i == k || 2*i == 20-k) && k <= 11) {
+                if ((2*i == k || 2*i == 20-k) && k <= 10) {
                     System.out.print("*");
                 } else {
                     System.out.print(" ");
