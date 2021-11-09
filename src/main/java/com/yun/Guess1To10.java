e302031616b573c9ec670564cc8817f9c69e7278
YunQi0102 <93147911+YunQi0102@users.noreply.github.com>
2021-11-09 21:48:39 +0800

    Update Guess1To10.java

--- src/main/java/com/yun/Guess1To10.java
+++ src/main/java/com/yun/Guess1To10.java
@@ -1,9 +1,27 @@
 package com.yun;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Guess1To10 {
     public static void main(String[] args) {
-        //產生1-10亂數
-        System.out.println("Hello");
-
+        Random random = new Random();
+        int secret = random.nextInt(10)+1;
+        System.out.println(secret);
+        System.out.println("Please enter a number 1-10:");
+        boolean play = true;
+        while (play) {
+            Scanner scanner = new Scanner(System.in);
+            String s = scanner.next();
+            int num = Integer.parseInt(s);
+            if (num > secret) {
+                System.out.println("Smaller");
+            } else if (num < secret) {
+                System.out.println("Bigger");
+            } else {
+                System.out.println("Bingo!");
+                play = false;
+            }
+        }
     }
 }
