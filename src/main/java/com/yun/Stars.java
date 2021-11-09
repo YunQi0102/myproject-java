7e1eccf1aa0245204bc360271dfe46c47a0a9d21
YunQi <mei970073@gmai.com>
2021-11-09 14:39:50 +0800

    initial commit

---
+++ src/main/java/com/yun/Stars.java
@@ -0,0 +1,17 @@
+package com.yun;
+
+public class Stars {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            for (int k = 0; k < 20; k++) {
+                if (i == 1 || i == 8) {
+                    System.out.print(" ");
+                } else {
+                    System.out.print("*");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
+
