bb0935799438146693d9515dadfaede97e4206dc
YunQi <mei970073@gmai.com>
2021-11-09 15:29:26 +0800

    initial commit

--- src/main/java/com/yun/StarLine.java
+++ src/main/java/com/yun/StarLine.java
@@ -2,9 +2,9 @@ package com.yun;
 
 public class StarLine {
     public static void main(String[] args) {
-        for (int i = 0; i < 10; i++) {
-            for (int k = 0; k < 20; k++) {
-                if (2*i == 19-k) {
+        for (int i = 0; i < 11; i++) {
+            for (int k = 0; k < 21; k++) {
+                if (2*i == 20-k || 2*i == k) {
                     System.out.print("*");
                 } else {
                     System.out.print(" ");
