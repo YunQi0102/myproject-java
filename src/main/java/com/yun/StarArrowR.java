53cc0920396a1aa1f5a60277e8e5126344979255
YunQi0102 <93147911+YunQi0102@users.noreply.github.com>
2021-11-10 00:36:30 +0800

    Create StarArrowR.java

---
+++ src/main/java/com/yun/StarArrowR.java
@@ -0,0 +1,16 @@
+package com.yun;
+
+public class StarArrowR {
+    public static void main(String[] args) {
+        for (int i = 0; i < 11; i++) {
+            for (int k = 0; k < 21; k++) {
+                if ((2*i == k || 2*i == 20-k) && k <= 11) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
