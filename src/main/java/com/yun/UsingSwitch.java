4df29976757b68c9b257f0875b919d76955e29f9
YunQi0102 <93147911+YunQi0102@users.noreply.github.com>
2021-11-23 13:28:54 +0800

    Add files via upload

---
+++ src/main/java/com/yun/UsingSwitch.java
@@ -0,0 +1,19 @@
+package com.fju;
+
+public class UsingSwitch {
+    public static void main(String[] args) {
+        int n = 5;
+        switch (n) {
+            case 1:
+                System.out.println("1");
+                break;
+            case 0:
+                System.out.println("0");
+                break;
+            default:
+                System.out.println("other");
+                break;
+        }
+        System.out.println("end");
+    }
+}
