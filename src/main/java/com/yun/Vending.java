ccaad7858ed3dc17d6e0cc58da926ce256156581
YunQi0102 <93147911+YunQi0102@users.noreply.github.com>
2021-11-30 14:37:15 +0800

    Update Vending.java

--- src/main/java/com/yun/Vending.java
+++ src/main/java/com/yun/Vending.java
@@ -7,35 +7,38 @@ public class Vending {
         Scanner scanner = new Scanner(System.in);
         int total = 0;
         boolean end = false;
-        int aPrice = 15;
-        int bPrice = 25;
+        int[] prices = {15, 25, 20};
+        System.out.println(prices.length);
+        for (int i = 0; i < prices.length; i++) {
+            System.out.println(prices[i]);
+        }
         while (!end) {
             System.out.println("Please put your coin (1/5/10):    Total:" + total);
             String s = scanner.next();
-            try {
-                int num = Integer.parseInt(s);
-                switch (num) {
-                    case 1:
-                    case 5:
-                    case 10:
-                        total = total + num;
-                        break;
-                    case 0:
-                        end = true;
-                        break;
-                    default:
-                        break;
-                }
-            } catch (NumberFormatException e) {
-                if (s.equals("a")) {
-                    System.out.println("water");
-                    if (total >= aPrice) {
-                        total = total - aPrice;
+            switch (s) {
+                case "1":
+                case "5":
+                case "10":
+                    int n = Integer.parseInt(s);
+                    total+=n; //total = total + 1;
+                    break;
+                case "a":
+                case "b":
+                case "c":
+                    int index = s.charAt(0)-97;
+                    if (total >= prices[index]) {
                         System.out.println("DON!");
+                        total-=prices[index];
                     } else {
                         System.out.println("BEEP!");
                     }
-                }
+                    break;
+                case "0":
+                    end = true;
+                    break;
+                default:
+                    System.out.println("Wrong answer");
+                    break;
             }
         }
         System.out.println("END");
