107b4b13cf671bd4cc91fc758d9cf06b88be9368
YunQi0102 <93147911+YunQi0102@users.noreply.github.com>
2021-12-07 15:28:37 +0800

    Add files via upload

---
+++ src/main/java/com/yun/Poker.java
@@ -0,0 +1,12 @@
+package com.fju;
+
+public class Poker {
+    public static void main(String[] args) {
+        char[] types = {'s','h','d','c'};
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 13; j++) {
+                System.out.println();
+            }
+        }
+    }
+}
