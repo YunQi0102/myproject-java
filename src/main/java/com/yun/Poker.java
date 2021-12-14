299c41a5edeafb09f986304c7d6e25c586948970
YunQi0102 <93147911+YunQi0102@users.noreply.github.com>
2021-12-14 14:07:11 +0800

    Update Poker.java

--- src/main/java/com/yun/Poker.java
+++ src/main/java/com/yun/Poker.java
@@ -2,10 +2,12 @@ package com.fju;
 
 public class Poker {
     public static void main(String[] args) {
-        char[] types = {'s','h','d','c'};
+        System.out.println((char)n);
+        String tt = "shdc";
+        //char[] types = {'s','h','d','c'};
         for (int i = 0; i < 4; i++) {
             for (int j = 0; j < 13; j++) {
-                System.out.print((j+1) +""+ types[i]);
+                System.out.print((j+1) +""+ tt.charAt(i));
                 System.out.print(" ");
             }
             System.out.println();
