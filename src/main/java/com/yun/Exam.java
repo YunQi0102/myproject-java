1ade6f0d8635d2b7355bb11949807444d0368cf1
YunQi0102 <93147911+YunQi0102@users.noreply.github.com>
2022-01-04 14:46:56 +0800

    Add files via upload

---
+++ src/main/java/com/yun/Exam.java
@@ -0,0 +1,12 @@
+package com.fju;
+
+public class Exam {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 6; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
