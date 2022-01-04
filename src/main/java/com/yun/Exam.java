9b3f0e5601af01dc0926989b1feeccf4bfa8b0e0
YunQi0102 <93147911+YunQi0102@users.noreply.github.com>
2022-01-04 15:09:38 +0800

    Update Exam.java

--- src/main/java/com/yun/Exam.java
+++ src/main/java/com/yun/Exam.java
@@ -4,7 +4,11 @@ public class Exam {
     public static void main(String[] args) {
         for (int i = 0; i < 5; i++) {
             for (int j = 0; j < 6; j++) {
-                System.out.print("*");
+                if (i == 0 || i == 4 || j == 0 || j == 5) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
             }
             System.out.println();
         }
